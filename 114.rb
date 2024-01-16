e0b9ac016bbdc7862b53e6902e3891b1a37250b9
matthewdillender <matthewdillender@gmail.com>
2024-01-16 11:50:22 -0800

    Create 114.rb

---
+++ 114.rb
@@ -0,0 +1,6 @@
+x = 40
+y = 3
+z = x + y 
+puts z  # z = 40 + 3 = 43
+x = 60
+puts z  # z = 43...the value of x changes from 40 to 60. The value change of x does not apply to all future values of z. The value of z remains the same due to initial set value of x.
