00b1681bac301d72a173aae3962dcb7b4c68096b
matthewdillender <matthewdillender@gmail.com>
2024-01-16 13:40:20 -0800

    Create 124.rb

---
+++ 124.rb
@@ -0,0 +1,17 @@
+dog = 7
+if dog < 10
+    puts "-1"
+elsif dog > 10
+    puts "1"
+else 
+    puts "0"
+end
+
+x = 100
+if x < 5
+    puts "0"
+elsif x < 100
+    puts "2"
+else 
+    puts "1"
+end
