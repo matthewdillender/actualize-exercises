e5447a5ce64cb4978c7f73f6050b1eef963e298d
matthewdillender <matthewdillender@gmail.com>
2024-01-16 12:37:16 -0800

    Update 121.rb

--- 121.rb
+++ 121.rb
@@ -1,7 +1,7 @@
-puts 42.765.round #rounds to closest whole integer
-puts 42.765.ceil #rounds to greater value integer
-puts 42.765.floor #rounds to lower value integer
+puts 42.765.round #rounds to closest whole Integer
+puts 42.765.ceil #rounds to greater value Integer
+puts 42.765.floor #rounds to lower value Integer
 puts "Hello".downcase #converts string to all lower case letters
 puts 999.to_s + "999" #converts to string
-puts "999".to_i + 999 #converts to integer
-puts "999".to_i + "999" #string cannot be coerced into integer
+puts "999".to_i + 999 #converts to Integer
+puts "999".to_i + "999" #string cannot be coerced into Integer
