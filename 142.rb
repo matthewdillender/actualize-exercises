894440b92c353cb6c2422902a7ec507a1c010db4
matthewdillender <matthewdillender@gmail.com>
2024-01-18 11:59:09 -0800

    Create 142.rb

---
+++ 142.rb
@@ -0,0 +1,16 @@
+# Question 1
+
+teams = ["Bears", "Bills", "Bucks", "Blues"]
+index = 0
+while index < teams.length
+    puts teams[index] 
+    index = index + 1
+end
+
+# Question 2
+numbers = [5, 15, 20, 60]
+index = 0
+while index < numbers.length
+    puts numbers[index] * 3
+    index = index + 1
+end
