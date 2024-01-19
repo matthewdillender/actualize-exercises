5bd6834e36f29da50662dc9a9d93fd60c0a9d4ac
matthewdillender <matthewdillender@gmail.com>
2024-01-19 11:59:58 -0800

    Create 152.rb

---
+++ 152.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+
+def product_of_three_inputs(value_a, value_b, value_c)
+    return value_a * value_b * value_c
+end
+
+output1 = product_of_three_inputs(2, 4, 6)
+
+puts output1
+
+# QUESTION 2
+
+# Explanation: Brackets and "(number)" statement created a single value input.
+def add_three_numbers(number1, number2, number3)
+    return number1 + number2 + number3
+  end
+  
+  sum = add_three_numbers(3, 2, 7)
+  puts sum
