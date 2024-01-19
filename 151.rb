7ee3920111fd4c929f196d4a786a9937abe1d87a
matthewdillender <matthewdillender@gmail.com>
2024-01-19 11:17:17 -0800

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,26 @@
+
+puts "#######                             "
+puts "   #    #    #   ##   #    # #    # "
+puts "   #    #    #  #  #  ##   # #   #  "
+puts "   #    ###### #    # # #  # ####   "
+puts "   #    #    # ###### #  # # #  #   "
+puts "   #    #    # #    # #   ## #   #  "
+puts "   #    #    # #    # #    # #    # "
+puts " "                                   
+puts "#     #               "
+puts " #   #   ####  #    # "
+puts "  # #   #    # #    # "
+puts "   #    #    # #    # "
+puts "   #    #    # #    # "
+puts "   #    #    # #    # "
+puts "   #     ####   ####  "
+puts " "
+
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+  end
+
+print_lyrics  
