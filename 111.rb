637c3aed75f3d414a602bf3ed4e18665bf544c00
matthewdillender <matthewdillender@gmail.com>
2024-01-19 20:50:29 -0800

    Update 111.rb

--- 111.rb
+++ 111.rb
@@ -1,11 +1,15 @@
-puts 4
-puts 30
-puts 12
-puts 983
+# QUESTION 1
+
+puts 2 * 2
+puts 90 / 3
+puts 8 + 4
+puts 986 - 3
+
+# QUESTION 2
+
 puts 333 + 333
 puts 200 - 100
 puts 6 * 8
 puts 99 / 11
-puts 678 x 211  #syntax error, unexpected tIDENTIFIER, expecting end-of-input
-puts 678 x 211  #use of "x", instead of "*"
-
+puts 25 \ 5     # use "/" for division not "\"
+puts 9 x 9      # use "*" for multiplication not "x"
