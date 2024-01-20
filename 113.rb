3e8868c5f57a8323c923a4d7365c7424dfc12c52
matthewdillender <matthewdillender@gmail.com>
2024-01-19 21:01:38 -0800

    Update 113.rb

--- 113.rb
+++ 113.rb
@@ -1,12 +1,18 @@
+# Question 1
+
 gmac2 = 60
 lroc3 = 25
 spoc4 = 100
 puts gmac2 + lroc3 + spoc4
+
+# Question 2
+
+
 bob7 = 50
-bob1 = 50
-bob3 = 50
-bob2 = 50
+bob = 50        # "7bob" no integers first in variable names
+boba = 50       # "bob@" not allowed 
+@bob = 50
 bob_ = 50
 _bob = 50
-bob5 = 50
-bob4 = 50
+bobs = 50       # "bob$" not allowed
+$bob = 50
