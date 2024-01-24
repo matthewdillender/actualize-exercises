a9a4fc5449829976762a05ed7cd5d419e76790a5
matthewdillender <matthewdillender@gmail.com>
2024-01-24 11:19:57 -0800

    Add exercise

---
+++ 232.rb
@@ -0,0 +1,15 @@
+# Question 1
+
+number = 1980
+while number <= 2020
+    puts number      
+    number = number + 1
+end
+
+# Question 2
+
+count = 1
+while count <= 10
+    puts count
+    count = count + 1
+end
