1e3d8d851148b93a8f8d9bd0c8da311fdfa145b4
matthewdillender <matthewdillender@gmail.com>
2024-01-24 10:44:07 -0800

    Add week 2 exercises

---
+++ 222.rb
@@ -0,0 +1,26 @@
+# QUESTION 1
+
+puts "Please enter your name."
+name = gets.chomp
+  if name == "Martin"
+    puts "Hi Martin!"
+  else 
+    puts "You're not Martin!"
+  end
+  
+
+# QUESTION 2
+
+x = 1000
+  if x < 5
+    puts 0
+  elseif x < 10
+    puts 1
+  else 
+    puts 2
+  end
+
+
+# git add --all
+# git commit -m "Add practice problem"
+# git push origin main
