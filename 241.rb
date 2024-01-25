b2ab884f572a22ac70dc2fcb3542122594371872
matthewdillender <matthewdillender@gmail.com>
2024-01-25 11:26:24 -0800

    Add practice solution

---
+++ 241.rb
@@ -0,0 +1,11 @@
+# DELIBERATE PRACTICE REFLECTION
+
+# Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+memory = ["input", "encoding", "retention", ]
+    memory.push"retrieval"
+    memory.push"decoding"
+p memory
+
+# I remembered this one as I had recently been doing another exercise with arrays.
+# I plan to choose deliberate practice questions that are relevant to daily topics.
