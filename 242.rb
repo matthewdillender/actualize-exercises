4d0eb5a72116d98168b73797711c209b50ef71c2
matthewdillender <matthewdillender@gmail.com>
2024-01-25 12:15:15 -0800

    Add exercise solutions

---
+++ 242.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+
+clouds = ["cirrus", "status", "nimbus"]   #array
+    clouds.push("cumulus")
+puts clouds
+
+
+# QUESTION 2
+
+ratings = {"berry" => 50, "citrus" => 20}
+ratings["melon"] = 30
+pp ratings
---
+++ 243.rb
@@ -0,0 +1,13 @@
+# QUESTION 1
+
+product = {:name => "Jacket", :price => 49.99, :color => "red"}
+puts product[:name] # the output is Jacket
+puts product[:price] # the output is 49.99
+
+
+# QUESTION 2
+
+fruits = {:coconut => 1, :apple => 2, :banana => 3}
+fruits[:pears] = 3 
+pp fruits
+
