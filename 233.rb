42ee1a41bf0440d062a2b86a9fa3ff8b37fb898e
matthewdillender <matthewdillender@gmail.com>
2024-01-25 11:07:03 -0800

    Add practice corrections

--- 233.rb
+++ 233.rb
@@ -1,19 +1,37 @@
 # Question 1
 
+# numbers = [4, 3, 1, 5]        # while loop
+# new_numbers = []
+# index = 0
+# while index < numbers.length
+#   number = numbers[index]
+#   new_numbers.push(number + 5)
+#   index = index + 1
+# end
+# pp new_numbers
 
-numbers = [4, 3, 1, 5]          # ( ".times" loop)
-
+numbers = [4, 3, 1, 5]          # ".times" loop
 new_numbers = []
 index = 0 
 numbers.length.times do 
     number = numbers[index]
-    new_numbers.push(number)
-    puts number * 1
+    new_numbers.push(number + 5)
     index = index + 1
+  end
+pp new_numbers
+
+
+numbers = [4, 3, 1, 5]           # ".each" loop
+new_numbers = []
+numbers.each do |number|
+  new_numbers.push(number + 5)
 end
+pp new_numbers
 
 
+# QUESTION 2
 
-numbers = [4, 3, 1, 5]            # ( ".each" loop)
-puts numbers.each { |each| }
-
+colors = ["red", "green", "blue"]
+colors.each do |color|
+  puts color.upcase
+end
