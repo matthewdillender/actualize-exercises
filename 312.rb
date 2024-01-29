9b771745cceb83165659e28c157464ef486eddd8
matthewdillender <matthewdillender@gmail.com>
2024-01-29 11:00:58 -0800

    Add exercise solutions

--- 312.rb
+++ 312.rb
@@ -5,11 +5,11 @@
 index = 0
 numbers = [4, 53, 22, 3, 9, 7, 12]
 
-while index <= numbers.length
+while index < numbers.length
   number = numbers[index]
   index = index + 1
   if number > 10
-    puts number
+    pp number
   end
 end
 
