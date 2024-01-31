b8f639250ba475e821b8e286a0803cfd4e7c7bce
matthewdillender <matthewdillender@gmail.com>
2024-01-31 09:39:47 -0800

    Add exercise solution

---
+++ 333.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+
+numbers = []
+5.times do
+  puts "Please enter a number."
+  numbers.push(gets.chomp.to_i)
+end
+
+mean = 0
+index = 0
+while index < numbers.length
+  mean = mean + numbers[index] / 5
+  index = index + 1
+end
+p mean
