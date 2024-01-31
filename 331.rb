2b82e3fdaa9adecdf36636a7087793e79b6606b5
matthewdillender <matthewdillender@gmail.com>
2024-01-31 09:43:17 -0800

    Add exercise solution

---
+++ 331.rb
@@ -0,0 +1,10 @@
+# Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+names = ["Jon", "Bill", "Ted"]
+index = 0
+while index < names.length
+  puts names[index]
+  index = index + 1
+end
+
+# I was able to remember this solution with ease.
