b3b3b66956cf6be84f0a619606340d6fda93ab05
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-02-02 16:01:14 -0800

    Add practice solutions

--- 333.rb
+++ 333.rb
@@ -13,3 +13,20 @@ while index < numbers.length
   index = index + 1
 end
 p mean
+
+
+
+# numbers = []
+# 5.times do
+#   puts "Please enter a number."
+#   numbers.push(gets.chomp.to_i)
+# end
+
+# mean = 0
+# index = 0
+# while index < numbers.length
+#   mean = mean + numbers[index] / 5
+#   mean = mean
+#   index = index + 1
+# end
+# p mean
---
+++ 351.rb
@@ -0,0 +1,16 @@
+1. Write a Song class with attributes for name, artist, and duration.
+
+class Song
+  def initialize(name, artist, duration)
+    @name = name
+    @artist = artist
+    @duration = duration
+  end
+end
+
+song = Song.new("Waiting Room", "Fugazi", "3:50")
+pp song
+
+# Yes, I mostly remembered the solution. This was straight forward. 
+# My plan is to assess where I get stuck in each category of practice problem and do one problem from each category daily.
+ 
