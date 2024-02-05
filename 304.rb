83d97718a615f71f6536c6bcd684a244e2df9b4b
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-02-05 14:50:11 -0800

    Add linkedin profile

---
+++ 304.rb
@@ -0,0 +1,8 @@
+# Follow this document to continue to develop linkedin profile...15-20 min per day!
+
+# https://docs.google.com/document/d/1df31q78ZlLuv-1sCQw-p61p-6Tfewf8seuQ5XhE99pM 
+
+
+# linkedin.com link:
+
+https://www.linkedin.com/in/matthew-dillender-07114b2b3/
