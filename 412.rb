99dd4ddaf1d88e4c219c6b604ea3e411c528e8a2
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-02-06 10:28:21 -0800

    Add exercise solutions

--- 412.rb
+++ 412.rb
@@ -3,41 +3,17 @@
 # Then run the method and print the result.
 
 
-# def sum(number1, number2, number3, number4)
-#   return number1 + number2 + number3 + number4
-# end
 
-# pp sum(50, 200, 300, 40)                # Method that satisfies problem without an array...how to..
-
-         
-# def sum_of(numbers)
-# index = 0 
-#   while index < num_array.length
-#     sum = 0 + numbers[index]                      
-#     index = index + 1
-#   end
-#  return sum 
-# end
-
-# puts sum
-
-     # The correct order of commands is extremely difficult...
-
-
-     # Stuck...
-
-# ChatGPT:
-
-def calculate_sum(numbers)
-    sum = 0                         # Need to revisit each
-    numbers.each do |num|           # Need to revisit how to use pipes...
-      sum += num
-    end
-    sum                             # Is this the same as "return sum"?
+def add_sum(numbers)
+  sum = 0                         
+  numbers.each do |num|           
+  sum = sum + num
   end
+return sum                             
+end
   
-  # Example usage:
-  numbers_array = [1, 2, 3, 4, 5]
-  result = calculate_sum(numbers_array)
+
+  array_of_numbers = [50, 60, 40, 20, 30]
+  total = add_sum(array_of_numbers)
   
-  puts "The sum of the array #{numbers_array} is: #{result}"    # Negative self talk is not useful...revisit.
+  puts "The sum of the array #{array_of_numbers} is #{total}"    
