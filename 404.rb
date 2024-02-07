e1b1aa780dd29ad1ee5e6c003d709e95f66f37a1
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-02-06 19:47:11 -0800

    Add 404.rb

---
+++ 404.rb
@@ -0,0 +1,10 @@
+
+Version 4 is my schedule of choice.
+I will be able to easily modify version 4 with an earlier start and later afternoon session.
+My desire is to have 3 projects I am proud of completed by June 19th. 
+This schedule is the one that will likely lead to my first job as a web developer.
+Getting my first job as a web developer ASAP is the goal. 
+
+
+
+https://docs.google.com/document/d/1T0Cnv-vHNJdfhyrdf2dAc9Tz4wD-AcICgEcsvaVkGHc/edit
