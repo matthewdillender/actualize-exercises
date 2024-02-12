e590eb35445d41a3cea7f00d9448714f13f550f9
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-02-12 11:42:18 -0800

    Add exercise solutions

---
+++ 511.rb
@@ -0,0 +1,16 @@
+# 10. Write a method that takes in a number and returns the number times 10 plus 30. Then run the method and print the result.
+
+def times_plus(number)
+    return number * 10 + 30
+end
+
+new_num = times_plus(number)
+puts times_plus(10)
+
+# Were you able to easily solve the problem from memory?
+# Yes, but I forgot the "end"! Doh!
+
+# If so, describe the next skill you will start to practice tomorrow.
+# Loops with more challenging method expressions.
+
+
